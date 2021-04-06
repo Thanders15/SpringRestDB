bd8075d7d612c0ef29351056fe4bb803f361f33a
Thanders15 <48353618+Thanders15@users.noreply.github.com>
2021-04-07 00:13:00 +0200

    Implementation JWT token, endpoint filter
    Implementacja JWT tokena, dodanie autoryzacji endpointów

--- demo/src/main/java/SpringDB/Config/RestAuthSuccesHandler.java
+++ demo/src/main/java/SpringDB/Config/RestAuthSuccesHandler.java
@@ -1,6 +1,10 @@
 package SpringDB.Config;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -8,12 +12,27 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @Component
 public class RestAuthSuccesHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    private final long validityTime;
+    private final String secret;
+
+    public RestAuthSuccesHandler(@Value("${jwt.validityTime}") long validityTime,@Value("${jwt.validityTime}") String secret) {
+        this.validityTime = validityTime;
+        this.secret = secret;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        clearAuthenticationAttributes(request);
+
+        UserDetails main = (UserDetails) authentication.getPrincipal();
+        String token = JWT.create()
+                .withSubject(main.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + validityTime))
+                .sign(Algorithm.HMAC256(secret));
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
--- demo/src/main/java/SpringDB/Config/SecurityConfig.java
+++ demo/src/main/java/SpringDB/Config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package SpringDB.Config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import javax.sql.DataSource;
@@ -19,12 +25,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final ObjectMapper objectMapper;
     private final RestAuthFailureHandler failureHandler;
     private final RestAuthSuccesHandler succesHandler;
+    private final String secret;
 
-    public SecurityConfig(DataSource dataSource, ObjectMapper objectMapper, RestAuthFailureHandler failureHandler, RestAuthSuccesHandler succesHandler) {
+    public SecurityConfig(DataSource dataSource, ObjectMapper objectMapper, RestAuthFailureHandler failureHandler,
+                          RestAuthSuccesHandler succesHandler, @Value("${jwt.secret}") String secret) {
         this.dataSource = dataSource;
         this.objectMapper = objectMapper;
         this.failureHandler = failureHandler;
         this.succesHandler = succesHandler;
+        this.secret = secret;
     }
 
     @Override
@@ -47,9 +56,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated().and().formLogin().permitAll()
                 .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .addFilter(autoticationFilter())
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userDetailsService(), secret))
                 .exceptionHandling()
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and().headers().frameOptions().disable();
     }
     public JsonAuthenticationFilter autoticationFilter() throws Exception{
         JsonAuthenticationFilter autoticationFilter = new JsonAuthenticationFilter(objectMapper);
@@ -58,5 +71,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         autoticationFilter.setAuthenticationFailureHandler(failureHandler);
         autoticationFilter.setAuthenticationManager(super.authenticationManager());
         return autoticationFilter;
+    }
+    @Bean
+    public UserDetailsManager userDetailsManager(){
+        return new JdbcUserDetailsManager(dataSource);
     }
 }
